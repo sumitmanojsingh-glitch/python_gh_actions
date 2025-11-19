bc6faa28fc73dcfd08557ffe575542b0ad7f33c5
sumitmanojsingh-glitch <sumitmanojsingh@gmail.com>
2025-11-19 07:42:04 +0530

    Starting workflow

--- demo.py
+++ demo.py
@@ -1,1 +1,8 @@
-print("Hello buddy")
+menu_items=["lemon","ginger"]
+customer_name=["sumit","sujeet"]
+
+for index,order in enumerate(menu_items,start=1):
+    print (f"Chai {index} : {order}")
+
+for menu,customer in zip(menu_items,customer_name):
+    print(f"chai {menu}: {customer}")
